2af2362aaf1a523570a02421db567dec9f62d342
smohadi <33235919+smohadi@users.noreply.github.com>
2017-11-25 19:48:23 -0500

    Update vdtp.py

--- vdtp.py
+++ vdtp.py
@@ -4,7 +4,7 @@ import pickle
 from random import *
 import struct
 import time
-
+seqSent = []
 # Required functions
 # Send_packet
 # Receive_packet
@@ -45,23 +45,31 @@ def fragmentData(serialData,reliable):
         header = struct.pack('BB',flowId,reliable << 1 + 1)
         #print len(header)
         retBuf = [header + serialData]
-
     return retBuf
 
 def ackthread(ack,addr,sk):
     ackData = pickle.dumps(ack)
-    ackList = fragmentData(ackData,0)
+    ackList = fragmentData(ackData,1)
     for item in ackList:
         sk.sendto(item,addr)
     ack = []
     return 0
 
+def retransmission()
+    
+    return 0
+
 def send(data,addr,reliable=0):
+    
     serialData = pickle.dumps(data)
     print 'Original Data Length:',len(serialData)
 
     # Fragment application layer packet to 1000 byte chunks
     fragDataList = fragmentData(serialData,reliable)
+    
+    
+    flowId,seq,reliable,lastFrag = extractHeader(data[0:2])
+    Global seqSent = seqSent.append(seq)
 
     # Attach header to individual messages
     # Send individual messages
@@ -85,7 +93,6 @@ def extractHeader(hdr):
     lastFrag = seqHdr & 1
     reliable = (seqHdr >> 1) & 1
     seq = (seqHdr >> 2) & 1
-
     return flowId,int(seq),reliable,lastFrag
 
 def recv_sock(addr):
@@ -104,26 +111,43 @@ def recv_sock(addr):
         #print length
         flowId,seq,reliable,lastFrag = extractHeader(data[0:2])
         ack.append(seq)
+        
+        if reliable==1
+           if flowIdList.has_key((addr,flowId)):
+                flowIdList[(addr,flowId)][seq] = data[2:length]
+            else:
+                flowIdList[(addr,flowId)] = {}
+                flowIdList[(addr,flowId)][seq] = data[2:length]
 
+            if lastFrag == 1:
+                for x in range(0,64):
+                    if flowIdList[(addr,flowId)].has_key(x):
+                        output += flowIdList[(addr,flowId)][x]
+                    else:
+                        del flowIdList[(addr,flowId)]
+                        break
+                returnBuf = pickle.loads(output) 
+            
+        else
         # In order delivery
-        if flowIdList.has_key((addr,flowId)):
-            flowIdList[(addr,flowId)][seq] = data[2:length]
-        else:
-            flowIdList[(addr,flowId)] = {}
-            flowIdList[(addr,flowId)][seq] = data[2:length]
+            if flowIdList.has_key((addr,flowId)):
+                flowIdList[(addr,flowId)][seq] = data[2:length]
+            else:
+                flowIdList[(addr,flowId)] = {}
+                flowIdList[(addr,flowId)][seq] = data[2:length]
 
-        if lastFrag == 1:
-            for x in range(0,64):
-                if flowIdList[(addr,flowId)].has_key(x):
-                    output += flowIdList[(addr,flowId)][x]
-                else:
-                    del flowIdList[(addr,flowId)]
-                    break
-            returnBuf = pickle.loads(output)
-            output = ''
-            return returnBuf,addr
-        if time.time()>close_time
-            ackthread(ack,addr,sk)
+            if lastFrag == 1:
+                for x in range(0,64):
+                    if flowIdList[(addr,flowId)].has_key(x):
+                        output += flowIdList[(addr,flowId)][x]
+                    else:
+                        del flowIdList[(addr,flowId)]
+                        break
+                returnBuf = pickle.loads(output)
+                output = ''
+                return returnBuf,addr
+            if time.time()>close_time
+                ackthread(ack,addr,sk)
 
 
 
